c0d06d621513ace35251800d1b561903f2ed4e1f
ElisaLissandra <elisa_lissandra@outlook.com>
2022-01-19 18:20:49 -0300

    criando controller

---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section ('title', 'Criar evento')
+
+@section('content')
+
+<h1>Crie um evento</p>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,24 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+use App\Http\Controllers\EventController;
 
-    $nome = "Matheus";
-    $idade = 29;
-
-    $arr = [1,2,3,4,5];
-
-    $nomes = ["Matheus", "Maria", "João", "Saulo"];
-
-    return view('welcome',
-        [
-            'nome' =>$nome,
-            'idade' => $idade,
-            'profissao' => "Programador",
-            'arr' => $arr,
-            'nomes' => $nomes
-        ]);
-});
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
 
 Route::get('/contact', function () {
     return view('contact');
